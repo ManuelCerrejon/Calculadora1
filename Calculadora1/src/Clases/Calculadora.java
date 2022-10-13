3f5633a8e73387e0684a3f90c1afb6046ac341df
manuc <manuc@LAPTOP-1MM8CT3Q>
2022-10-13 18:15:36 +0200

    Calculadora

--- Calculadora1/src/Clases/Calculadora.java
+++ Calculadora1/src/Clases/Calculadora.java
@@ -1,6 +1,26 @@
 package Clases;
 
 public class Calculadora {
+	public static int suma(int a, int b) {
+		return a+b;
+	}
 	
+	public static int resta(int a, int b) {		
+		return a-b;
+	}
+	
+	public static int multiplicacion(int a, int b) { 
+		return a*b;
+	}
+	
+	public static int division(int a, int b) {
+		if (b==0)
+		{
+			return -1;
+		}else {
+			return a/b;
+		}
+		
+	}
 
 }
